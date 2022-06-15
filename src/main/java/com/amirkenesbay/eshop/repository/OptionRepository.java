7d4463c797c7765faabe589282fab0b229e2262e
Amir Kenesbay <amirkenesbay7@gmail.com>
2022-06-15 22:56:18 +0600

    Added repository for option

---
+++ src/main/java/com/amirkenesbay/eshop/repository/OptionRepository.java
@@ -0,0 +1,7 @@
+package com.amirkenesbay.eshop.repository;
+
+import com.amirkenesbay.eshop.entity.Option;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OptionRepository extends JpaRepository<Option, Long> {
+}
