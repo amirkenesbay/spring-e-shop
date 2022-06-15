603c531c54072a475f42a6d71fddba4471dc7ab4
Amir Kenesbay <amirkenesbay7@gmail.com>
2022-06-15 22:56:52 +0600

    Added repository for value

---
+++ src/main/java/com/amirkenesbay/eshop/repository/ValueRepository.java
@@ -0,0 +1,7 @@
+package com.amirkenesbay.eshop.repository;
+
+import com.amirkenesbay.eshop.entity.Value;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ValueRepository extends JpaRepository<Value, Long> {
+}
